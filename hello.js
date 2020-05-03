e9db9468c6c7500d4fe216b49fcec96cc0bec9b6
AlexanderAndr <60938724+AlexanderAndr@users.noreply.github.com>
2020-05-03 16:51:08 +0300

    Task6

---
+++ hello.js
@@ -0,0 +1,349 @@
+var posts = [
+ {
+     id: '1',
+     description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
+     createdAt: new Date('2021-03-17T23:00:00'),
+     author: 'Иванов Иван',
+     photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+ },
+  {
+      id: '2',
+      description: 'Второй пост создан',
+      createdAt: new Date('2020-04-19T21:00:00'),
+      author: 'Петров Сергей',
+      photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+  },
+  {
+      id: '3',
+      description: 'Третий пост создан',
+      createdAt: new Date('2020-04-19T21:00:00'),
+      author: 'Mr.Snow',
+      photoLink: 'Like.png'
+  },
+  {
+      id: '4',
+      description: 'Четвёртый пост создан',
+      createdAt: new Date('2020-04-15T21:00:00'),
+      author: 'Mr.Snow',
+      photoLink: 'Like.png'
+  },
+  {
+      id: '5',
+      description: 'Пятый пост создан',
+      createdAt: new Date('2020-01-19T21:00:00'),
+      author: 'Mr.Slow',
+      photoLink: 'Like.png'
+  },
+  {
+      id: '6',
+      description: 'В Беларуси зарегистрировано 8773 случая коронавируса',
+      createdAt: new Date('2020-4-24T22:30:00'),
+      author: 'Mr.Snow',
+      photoLink: 'Like.png' 
+  },
+  {
+      id: '7',
+      description: 'Лукашенко подписал указ "О поддержке экономики"',
+      createdAt: new Date('2020-4-24T22:32:00'),
+      author: 'Sidorov',
+      photoLink: 'Like.png'
+  },
+   {
+       id: '8',
+       description: 'Лукашенко подписал указ об учебном дне в школах 1 мая',
+       createdAt: new Date('2020-4-24T22:32:00'),
+       author: 'Sidorov',
+       photoLink: 'Like.png'
+   },
+    {
+        id: '10',
+        description: 'Голикова раскрыла статистику смертности от коронавируса по возрастам',
+        createdAt: new Date('2020-4-24T21:32:00'),
+        author: 'Sidorov K.',
+        photoLink: 'Like.png'
+    },
+    {
+        id: '11',
+        description: 'Голикова раскрыла статистику смертности от коронавируса по возрастам',
+        createdAt: new Date('2020-4-24T21:32:00'),
+        author: 'Mr.Snow',
+        photoLink: 'Like.png'
+    },
+    {
+        id: '12',
+        description: 'Голикова раскрыла статистику смертности от коронавируса по возрастам',
+        createdAt: new Date('2020-4-24T21:32:00'),
+        author: 'Mr.Snow',
+        photoLink: 'Like.png'
+    },
+    {
+        id: '13',
+        description: 'Голикова раскрыла статистику смертности от коронавируса по возрастам',
+        createdAt: new Date('2020-4-24T21:32:00'),
+        author: 'Mr. Snow',
+        photoLink: 'Like.png'
+    },
+    {
+        id: '14',
+        description: 'Лукашенко заявил о безобразном отношении к природе',
+        createdAt: new Date('2020-4-24T21:11:00'),
+        author: 'Mr',
+        photoLink: 'Like.png'
+    },
+     {
+         id: '15',
+         description: 'Лукашенко заявил о безобразном отношении к природе',
+         createdAt: new Date('2020-4-24T21:11:00'),
+         author: 'Mr. Snow',
+         photoLink: 'Like.png'
+     },
+   {
+       id: '16',
+       description: 'Лукашенко заявил о безобразном отношении к природе',
+       createdAt: new Date('2020-4-24T21:11:00'),
+       author: 'Mr. Snow',
+       photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+   },
+     {
+         id: '17',
+         description: 'В РПЦ объяснили портреты Путина и Сталина на мозаике главного храма армии',
+         createdAt: new Date('2020-4-24T22:08:00'),
+         author: 'Mr. Snow',
+         photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+     },
+      {
+          id: '18',
+          description: 'В Беларусь прибыла гуманитарная помощь из Польши',
+          createdAt: new Date('2020-4-24T20:08:00'),
+          author: 'Mr. S',
+          photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+      },
+        {
+            id: '19',
+            description: 'Президент Литвы рассказал о разговоре с Лукашенко',
+            createdAt: new Date('2020-4-24T21:48:00'),
+            author: 'Mr. St',
+            photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        },
+         {
+             id: '20',
+             description: 'Путин упростил получение российского гражданства беларусами',
+             createdAt: new Date('2020-4-24T11:48:00'),
+             author: 'Mr. St',
+             photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+         },
+          {
+              id: '21',
+              description: 'Средняя зарплата в Беларуси в марте составила 1213,6 рублей',
+              createdAt: new Date('2020-4-24T11:48:00'),
+              author: 'Mr. wwSt',
+              photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+          },
+          {
+              id: '22',
+              description: 'Ученые обнаружили новый симптом коронавируса',
+              createdAt: new Date('2020-4-24T11:28:00'),
+              author: 'Mr.r St',
+              photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+          },
+            {
+                id: '23',
+                description: 'Ученые обнаружили новый симптом коронавируса',
+                createdAt: new Date('2020-4-24T11:28:00'),
+                author: 'Mr. wSt',
+                photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+            }
+];
+(function()
+{
+    debugger
+
+    function getPosts(number1, number2, filter)//Отсортировать пост.
+    {
+        if (number1 == undefined)
+            number1 = 0;
+        if (number2 == undefined)
+            number2 = 10;
+        if (filter == undefined) {
+            var temp = posts.slice(number1, number1 + number2);
+            temp.slice(number1, number1 + number2);
+            temp.sort(function (a, b) { return a.createdAt - b.createdAt })
+            console.log(temp);
+        }
+        else {
+
+            let arr = [];
+            for (var i = 0; i < posts.length; i++) {
+                if (filter.author == posts[i].author) {
+                    arr.push(posts[i]);
+                }
+            }
+            arr.sort(function (a, b) { return a.createdAt - b.createdAt })
+            console.log(arr);
+
+
+        }
+
+    }
+
+
+    function getPost(id)//пост по id
+    {
+        for (let i = 0; i < posts.length; i++)
+        {
+
+            if (id == posts[i].id) 
+                {
+                console.log(posts[i]);
+            }
+        }
+    }
+  
+
+    function validatePost(a)
+    {
+
+        var i = 0;
+        var k = 0;
+        var indicate = 0;       
+        if (typeof (a.createdAt) != "object") {
+
+            return false;
+            exit(0);
+
+        }
+        else
+        {
+            var temp_date = new Date();
+            temp_date = a.createdAt;
+            var n = temp_date.toDateString();
+        }
+        if (a.id == undefined || a.author == undefined || a.description == undefined)
+        {
+            return false;
+            exit(0);
+        }
+        if (typeof (a.id) != "string")
+        {
+            k++;
+        }
+        else
+            i++;
+
+        if (typeof (a.description) != "string")
+        {
+            k++;
+        }
+        else
+            i++;
+
+
+        while (indicate != 1)
+        {
+            if (typeof (n) != "string") {
+                k++;
+            }
+            else
+                i++;
+            indicate++;
+        }
+        if (typeof a.author != "string" || a.author=='')
+        {
+            k++;
+        }
+        else
+            i++;
+
+        if(i==4)
+        {
+            return true;
+        }
+
+        if (k > 0)
+            return false;
+        }
+
+
+
+    function addPost(b) {
+        if (validatePost(b) == true) {
+            posts.splice(0, 0, b);
+            return true;
+        }
+        else {
+            return false;
+            exit(0);
+        }
+    }
+
+
+
+    function editPost(id, c) {
+            var post = 0;
+            var k = 0;
+            for (var i = 0; i < posts.length; i++) {
+                if (id == posts[i].id) {
+                    post = posts[i];
+                    k = i;
+                }
+            }
+
+
+            if (c.description != undefined) {
+                post.description = c.description;
+            }
+            
+
+            if (c.createdAt != undefined) {
+                post.createdAt = c.createdAt;
+            }
+          
+
+            if (c.author != undefined) {
+                post.author = c.author;
+            }
+          
+
+
+            if (c.photoLink != undefined) {
+                post.photoLink = c.photoLink;
+            }
+
+
+
+            if (validatePost(post) == true) {
+                posts[k] = post;
+                return true;
+            }
+            else
+                return false;
+        }
+    
+
+    function removePost(id)//удалить пост по id
+    {
+        let temp=0;
+        for (let i = 0; i < posts.length; i++)
+        {
+
+            if (id == posts[i].id) {
+                posts.splice(i, 1);
+                return true;
+                break;
+            }
+            else
+                temp++;
+        }
+
+        if (temp == posts.length)
+            return false;
+
+    }
+
+    
+
+
+
+
+
+}())
